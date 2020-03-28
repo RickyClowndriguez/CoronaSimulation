d505b985b65f42ac408c6d616a17c757056360dd
S. Löffler <loeffler@physik.uni-kiel.de>
2020-03-28 03:41:56 +0100

    Even better

--- CoronaParser.py
+++ CoronaParser.py
@@ -2,35 +2,62 @@
 
 #Please make sure your local COVID-19 data repository is cloned to the same parent directory in which this repo is located. 
 
+import datetime
+from numpy import array
+
 path='./../COVID-19/csse_covid_19_data/csse_covid_19_time_series/'
 fname='time_series_covid19_*_global.csv'
 
-def parse(which='confirmed'):
+def parse(which=None):
+	if which==None:
+		which='confirmed'
 	f=path+fname.replace('*',which)
 	data={}
 	try:
 		fl=open(f,'r')
 		header=True
 		for line in fl:
+			if '\"' in line:
+				#fix South Korea...
+				line=fix_comma_in_name(line)
 			d=line.split(',')
 			d[-1]=d[-1].strip('\n')
 			if header:
-				data['dates']=d[4:]
+				dates=[]
+				for date in d[4:]:
+					m,d,y=date.split('/')
+					dates.append(datetime.date(int(y),int(m),int(d)))
+				data['dates']=array(dates)
 				header=False
 			else:
 				province=d[0]
 				region=d[1]
 				lat=d[2]
 				lon=d[3]
-				N_t=d[4:]
+				#print d
+				N_t=[float(val) for val in d[4:]]
 				if region in data.keys():
-					data[region]+=N_t
+					data[region]+=array(N_t)
 				else:
-					data[region]=N_t
+					data[region]=array(N_t)
 		fl.close()
 		return data
 	except Exception as e:
 		print 'Failed to load data!'
 		import traceback
 		traceback.print_exc()
+
+
+def fix_comma_in_name(line):
+	line=list(line)
+	i=0
+	while line[i]!='\"':
+		i+=1
+	j=len(line)-1
+	while line[j]!='\"':
+		j-=1
+	for k in range(i,j):
+		if line[k]==',':
+			line[k]='_'
+	return ''.join(line)
 
--- Main.py
+++ Main.py
@@ -1,5 +1,24 @@
 
 
+def load_model(pars=None):
+	if pars==None:
+		pars=[None for i in range(5)]
+	N0,dt,thr,qd,I0=pars
+	from Model import RK4Integrator
+	m=RK4Integrator()
+	if N0!=None:
+		m.N0=N0
+	if dt!=None:
+		m.dt=dt
+	if thr!=None:
+		m.thr=thr
+	if qd!=None:
+		m.qd=qd
+	if I0!=None:
+		m.I0=I0
+	m.integrate()
+	return m
+
 def load_data(which=None,countries=None,daterange=None):
 	from CoronaParser import parse
 	from numpy import logical_and
@@ -22,7 +41,40 @@ def load_data(which=None,countries=None,daterange=None):
 			data[country]=d[country][datemask]
 	return data
 
+def plot_data(data,fig=None,which=None,fname=None):
+	import matplotlib.pyplot as plt
+	if which==None:
+		which='Germany'
+	if not fig:
+		fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.set_xlabel('Days')
+	ax.set_ylabel('N(d)')
+	ax.plot(data[which])
+	ax.legend([which])
+	if fname:
+		fig.savefig(fname)
+	else:
+		plt.show()
+	return fig
 
+def plot_model(model,fig=None,fname=None):
+	import matplotlib.pyplot as plt
+	if not fig:
+		fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.plot(model.time, model.sus, label =  "susceptable")
+	ax.plot(model.time, model.inf, label =  "infected")
+	ax.plot(model.time, model.rec, label =  "recovered")
+	ax.axvspan(model.t0, model.t0+model.qd, alpha=0.5, color='green')
+	ax.set_xlabel("time in days")
+	ax.set_ylabel("ratio of total population")
+	ax.legend()
+	if fname:
+		fig.savefig(fname)
+	else:
+		plt.show()
+	return fig
 
 
 if __name__ == "__main__":
@@ -35,4 +87,7 @@ if __name__ == "__main__":
 	args=parser.parse_args()
 
 	d=load_data()
+	m=load_model()
+	fig=plot_data(d,fname='dataplot.pdf')
+	fig=plot_model(m,fname='modelplot.pdf')
 	#print d
